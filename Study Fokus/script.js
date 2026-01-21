d1d7117573bfc5d311790a6fde5f3db7320bff27
AdrielBTX <nbblackff@gmail.com>
2026-01-21 07:50:08 -0300

    Código atualizado 2
    20/01/2026

--- Study Fokus/script.js
+++ Study Fokus/script.js
@@ -1,6 +1,51 @@
 const abrirModal = document.getElementById("abrirModal");
 const fecharModal = document.getElementById("fecharModal");
 const modal = document.getElementById("modal");
+const inputMateria = document.getElementById("inputMateria");
+const btnCriar = document.querySelector(".criarMateria");
+const cardsContainer = document.querySelector(".cards")
+
+
+let corEscolhida = ""
+const botoesCor = document.querySelectorAll(".cor");
+
+botoesCor.forEach( botao  => {
+    botao.addEventListener("click", () =>{
+        corEscolhida = botao.classList[1]
+    botoesCor.forEach(b => b.classList.remove("ativo"))
+    botao.classList.add("ativo")
+});
+});
+// Evento para criar card
+btnCriar.addEventListener("click", () => {
+    // variavel input do valor
+    const nome  = inputMateria.value.trim();
+    // verificação 
+    if(nome === ""){
+        alert("Escreva algo!");
+        return ;
+    }
+    if(!corEscolhida){
+        alert("Escolha uma cor!")
+        return ;
+    }
+
+    const novoCard = document.createElement("div");
+    novoCard.classList.add("card", corEscolhida);
+    novoCard.innerHTML = `<div class="iconcard"> <img  class="excluirCard" src="imagens/lixo-icon.png" alt="Imagem logo do card"></img></div>
+        <h3 class="cardtitulo">${nome}</h3>
+    
+
+        ` ;
+        const btnExcluir = novoCard.querySelector(".excluirCard")
+        btnExcluir.addEventListener("click", () => {
+            novoCard.remove()
+        });
+        cardsContainer.appendChild(novoCard)
+        inputMateria.value = ""
+        corEscolhida = ""
+        botoesCor.forEach((b) => b.classList.remove("ativo"))
+});
 
 abrirModal.addEventListener("click", () => {
     modal.style.display = "flex";
@@ -16,3 +61,7 @@ modal.addEventListener("click", (e) => {
         modal.style.display = "none";
     }
 });
+
+function carrinho() {
+    
+}
